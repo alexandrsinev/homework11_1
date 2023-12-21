fa17fc1fab201fd5d151a39ca6988d169fa56504
Alexandr Sinev <sir.sineow@yandex.ru>
2023-12-21 23:16:33 +0300

    function additition and subtraction

--- funcs.py
+++ funcs.py
@@ -1,4 +1,9 @@
 def addition():
     """Складывает 2 числа"""
-    rez = 3 + 66
-    return rez
+    rez1 = 3 + 66
+    return rez1
+
+def subtraction():
+    """Вычитание одного числа из другого"""
+    rez2 = 66 - 3
+    return rez2
